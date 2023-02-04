f5b94354892002b3461898476f93326305e588fe
fre4i <vaskashow@gmail.com>
2023-02-04 22:00:55 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import command
+from aiogram.types import Message
+
+
+def telegram(name):
+    pass
+
+
+if __name__ == '__main__':
+    pass
